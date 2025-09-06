d799b43997369b92701fed663d70609dc89707cf
starrmomo <momohgbenuade36@gmail.com>
2025-09-06 18:54:09 +0100

    commit message

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const human = confirm ("are you human");
+console.log("human");
+const name = prompt("what is your name");
+console.log(name);
+let jobtitle = prompt("what is your job");
+console.log(jobtitle)
