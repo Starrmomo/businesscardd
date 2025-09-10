8853f68eea2470eac7f536f59a43d4bfb41011ca
starrmomo <momohgbenuade36@gmail.com>
2025-09-10 05:41:01 +0500

    commit message

---
+++ assignment3.js
@@ -0,0 +1,91 @@
+
+let Acct1 = {
+    Name: "ade",
+    Balance: 80000,
+    Currency: "NGN",
+    Type: "Savings",
+ };
+ 
+let Acct2 = {
+    Name: "idowu",
+    Balance: 20000,
+    Currency: "NGN" ,
+    Type: "Savings",
+ };
+ 
+let Acct3 = {
+    Name: "fatima",
+    Balance: 90000,
+    Currency: "NGN" ,
+    Type: "Savings",
+ };
+ 
+let Acct4 = {
+    Name: "starr",
+    Balance: 40000,
+    Currency: "NGN" ,
+    Type: "Checking",
+ };
+ 
+// Simulate deposits:  Add different deposit amounts to two accounts.
+// Show the new balances.
+let depositamount1 = 3000
+let depositamount2 = 6000
+ 
+//add depositamount1 to acct1
+Acct1.Balance += depositamount1
+console.log("The current balance1 is: " +   Acct1.Balance)
+ 
+Acct2.Balance += depositamount2
+console.log("The current balance2 is: " + Acct2.Balance)
+ 
+//Simulate withdrawals:
+// Attempt withdrawals from two accounts.
+// If the balance is insufficient, print:
+// "Withdrawal denied for <name>: insufficient funds"
+ 
+let withdrawalamount3 = 2000
+let withdrawalamount4 = 12000
+ 
+Acct3.Balance -= withdrawalamount3
+if (Acct3.Balance >= withdrawalamount3) {
+    console.log (Acct3.Balance -= withdrawalamount3)
+   
+} else {
+    console.log ( "insufficient balance")
+   
+}
+ 
+Acct4.Balance -= withdrawalamount4
+if (Acct4.Balance >= withdrawalamount4) {
+    console.log (Acct4.Balance -= withdrawalamount4)
+   
+} else {
+    console.log ( "insufficient balance")
+   
+}
+ 
+//Transfers:  Transfer money between two accounts only if they have the same currency.
+//  If currencies are different, print:  "Transfer failed: currency mismatch"
+ 
+let transferamount = 40000
+if (Acct1.Currency === Acct2.Currency ) {
+   Acct1.Balance -= transferamount
+   Acct2.Balance += transferamount
+   console.log ("The current balance of acct1 is: " + Acct1.Balance + "The current balance of acct2 is :" + Acct2.Balance)
+   
+} else {
+    console.log("Currency mismatch")
+   
+}
+//Monthly maintenance:
+// If account type is "Savings", add interest (e.g., 2% of balance).
+// If account type is "Checking", deduct fees (e.g., 50 units).
+ 
+
+ 
+
+ 
+
+ 
+ 
--- javascript.js
+++ javascript.js
@@ -1,6 +1,59 @@
-const human = confirm ("are you human");
+const human = confirm("are you human");
 console.log("human");
 const name = prompt("what is your name");
 console.log(name);
 let jobtitle = prompt("what is your job");
 console.log(jobtitle)
+
+
+
+
+let monthlyincome = 2000000;
+let dailyexpenses = 203023;
+let monthlyexpenses = 3543545;
+let saving = 324342;
+let spending = 232323;
+
+let left = monthlyincome - (dailyexpenses + monthlyexpenses + saving + spending);
+console.log(`debt i'm in is${left}`);
+
+
+
+
+let account1 = {
+    name: 'idowu',
+    balance: 1000000,
+    currency: 'NGN'
+};
+
+let account2 = {
+    name: 'ade',
+    balance: 500000,
+    currency: 'NGN'
+};
+
+let account3 = {
+    name: 'fatima',
+    balance: 700000,
+    currency: 'NGN'
+};
+
+
+account1.balance += 5000 + 50000;
+console.log(account1.balance);
+
+account2.balance -= 60000;
+console.log(account2.balance);
+
+account3.balance -= 50000;
+console.log(account3.balance);
+
+if (account1.balance >= account2.balance) {
+    console.log(`account1 has the highest`);
+}
+else (account1.balance <= account2balance)
+{
+    console.log(`account2 has the lowest`);
+}
+
+
